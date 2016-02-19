a3e8add3306e13869aec92c398b508d7bd6a6540
Christina L. Cole <christinalcole@gmail.com>
2016-02-19 12:08:41 -0500

    Adding error message for contraint on word length, unique letters

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1,10 +1,42 @@
+def three_letters?(input)
+  if (input.length<=3)
+   return true
+  else
+   return false
+  end
+end
+
+def distinct_letters?(input)
+  letter_array = input.chars
+  unique_letters = letter_array.uniq
+  if (unique_letters.length == letter_array.length)
+    return true
+  else
+    return false
+  end
+end
+
+def valid_input?(input)
+  if three_letters?(input) && distinct_letters?(input)
+    return true
+  else
+    return false
+  end
+end
+
+
 get '/' do
   erb :index
 end
 
 post '/' do
-  @word = params[:word]
-  redirect "/anagrams/#{@word}"
+  word = params[:word]
+  if valid_input?(word)
+    redirect "/anagrams/#{word}"
+  else 
+    @error = "Oh no!  Try again--but this time, stick to 3-letter words with no repeating letters."
+    erb :index
+  end
 end
 
 get '/anagrams/:word' do
